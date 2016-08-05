63a9fd36765a0f5e7ce23becd26a84b4087a37d8
youshujie <youshujie77@gmail.com>
2016-08-05 21:52:16 +0800

    tab js

--- js/way.js
+++ js/way.js
@@ -1,203 +1,141 @@
-//封装
-var getStyle = function(dom,attr){
-    return dom.currentStyle ? dom.currentStyle[attr] : getComputedStyle(dom, false)[attr];
-}
-
-var $ = function (event) {
-	return document.querySelector(event);
-}
-
-
-//飞机和回顶按钮的隐藏
-var tt = 500;
-    fix_l = $(".fix-l")
-    fix_r = $(".fix-r");
-
-window.onscroll = function(){  //兼容ie8
-    var t = document.documentElement.scrollTop || document.body.scrollTop;
-    	bottom_t = document.documentElement.offsetHeight || document.body.offsetHeight;
-    	bottom_b = document.documentElement.clientHeight || document.body.clientHeight;
-    	bottom = bottom_t - bottom_b - t;
-
-	if (bottom <= 10) {
-		backTop.style.bottom = (-bottom) + 30 + "px";
-	}else {
-		backTop.style.bottom = 30 + "px";
-	}
-
-    if(t < tt && getStyle(backTop,"opacity")!="0"){
-        backTop.style.opacity="0";
-    }else if(t > tt && getStyle(backTop,"opacity")=="0"){
-        backTop.style.opacity="1";
-    }else{
-        return false;
-    }
-
-    if(t < 1500 && getStyle(fix_l,"opacity")!="1"){
-        fix_l.style.opacity="1";
-    }else if(t > tt && getStyle(fix_l,"opacity")=="1"){
-        fix_l.style.opacity="0";
-    }else{
-        return false;
-    }
-
-    if(t < 1500 && getStyle(fix_r,"opacity")!="1"){
-        fix_r.style.opacity="1";
-    }else if(t > tt && getStyle(fix_r,"opacity")=="1"){
-        fix_r.style.opacity="0";
-    }else{
-        return false;
-    }
-}
-
-
-//回到顶部
-var backTop = $(".back"); //回顶按钮
-    backTop.addEventListener("click", back)
-function back() {
-	var	t = document.documentElement.scrollTop || document.body.scrollTop;
-		speed = 100;  //每次位移量
-		interval = 20
-	if (t > 0) {
-		document.documentElement.scrollTop = t - speed;
-		document.body.scrollTop = t - speed;
-		setTimeout(back, interval);
-	} 
-}
-
-//计时器
-var timer = [$(".day-l"), $(".day-r"), $(".hour-l"), $(".hour-r"), $(".min-l"), $(".min-r"), $(".sec-l"), $(".sec-r")]
-	startTime = new Date().getTime();
-	timeNew = new Date();
-    month = timeNew.getMonth() + 1;
-    year = timeNew.getFullYear()
-    day = 0;
-	count = 0;
-    stop = 0;
-
-function change(event, num){
-    if (num == 0) {
-        event.style.top = 0 + "px";
-    }
-    if (num == 1) {
-        event.style.top = -37 + "px";
-    }
-    if (num == 2) {
-        event.style.top = -73 + "px";
-    }
-    if (num == 3) {
-        event.style.top = -110 + "px";
-    }
-    if (num == 4) {
-        event.style.top = -147 + "px";
-    }
-    if (num == 5) {
-        event.style.top = -183 + "px";
-    }
-    if (num == 6) {
-        event.style.top = -220 + "px";
-    }
-    if (num == 7) {
-        event.style.top = -257 + "px";
-    }
-    if (num == 8) {
-        event.style.top = -293 + "px";
-    }
-    if (num == 9) {
-        event.style.top = -330 + "px";
-    }
-
-}
-
-if (month == 8 && year == 2016){
-    day = 31+5;
-}else if (month == 9 && year == 2016) {
-    day = 5;
-}else {
-	stop = 1
-}
-
-function timeChange(stop) {
-    count++;
-
-    var offset = new Date().getTime() - (startTime + count * 1000);
-        nextTime = 1000 - offset;
-        timeNew = new Date();
-        sec = timeNew.getSeconds();
-        min = timeNew.getMinutes();
-        hour = timeNew.getHours();
-        dayNew = day - timeNew.getDay();
-
-    if (nextTime < 0) nextTime = 0;
-
-    if (stop != 1) {
-		var time = [parseInt((dayNew%100)/10), dayNew%10, (2 - parseInt((hour%100)/10)), (4 - hour%10), (5 - parseInt((min%100)/10)), (9 - min%10), (5 - parseInt((sec%100)/10)), (9 - sec%10)]
-			length = timer.length
-
-		for (var i = 0; i < length; i++) {
-			change(timer[i], time[i])
-		}
-	    
-    }else{
-    	for (var i = 0; i < length; i++) {
-    		change(timer[i], 0);
-    	}
-    }
-    setTimeout(timeChange, nextTime);
-}
-setTimeout("timeChange(" + stop + ")", 1000);
-
-
-
-
-
-
-
-
-//游舒婕的
-  dormitory = $("#dormitory"),
-  necessary = $("#necessary"),
-  entrance = $("#entrance"),
-  traffic = $("#traffic"),
-  qq = $("#qq_group"),
-  dormitory_d = $("#dormitory_d"),
-  necessary_d = $("#necessary_d"),
-  entrance_d = $("#entrance_d"),
-  traffic_d = $("#traffic_d"),
-  qq_group_d = $("#qq_group_d");
+var doc = document,
+    dormitory = doc.querySelector("#dormitory"),
+    necessary = doc.querySelector("#necessary"),
+    entrance = doc.querySelector("#entrance"),
+    traffic = doc.querySelector("#traffic"),
+    qq = doc.querySelector("#qq_group"),
+    dormitory_d = doc.querySelector("#dormitory_d"),
+    necessary_d = doc.querySelector("#necessary_d"),
+    entrance_d = doc.querySelector("#entrance_d"),
+    traffic_d = doc.querySelector("#traffic_d"),
+    qq_d = doc.querySelector("#qq_group_d"),
+    surround = doc.querySelector(".surround"),
+    playCqupt = doc.querySelector(".play_cqupt"),
+    thrnav = doc.querySelector("#third_nav_three"), 
+    fivnav = doc.querySelector("#third_nav_five"), 
+    life = doc.querySelector("#com_life"),
+    food = doc.querySelector("#love_food"),
+    scenery = doc.querySelector("#love_fengjin"),
+    life_d = doc.querySelector("#life_d"),
+    food_d = doc.querySelector("#food_d"),
+    scenery_d = doc.querySelector("#fengjin_d");
 
 dormitory.addEventListener("click",function () {
-  dormitory_d.style.display = "block";
-  entrance_d.style.display = "none";
-  traffic_d.style.display = "none";
-  necessary_d.style.display = "none";
-  qq_group_d.style.display = "none"; 
+    show(dormitory_d);
+    dormitory.className = "strategy_way_click";
+    var dod = [entrance_d,traffic_d,necessary_d,qq_d,life_d,food_d,scenery_d];
+    notshow(dod);
+    var dor = [necessary,entrance,traffic,qq];
+    changename(dor);
 });
+
 necessary.addEventListener("click",function () {
-  necessary_d.style.display = "block";
-  entrance_d.style.display = "none";
-  traffic_d.style.display = "none";
-  dormitory_d.style.display = "none";
-  qq_group_d.style.display = "none"; 
+    show(necessary_d);
+    necessary.className = "strategy_way_click";
+    var nnd = [entrance_d,traffic_d,dormitory_d,qq_d,life_d,food_d,scenery_d];
+    notshow(nnd);
+    var nn = [dormitory,entrance,traffic,qq];
+    changename(nn);
 });
+
 entrance.addEventListener("click",function () {
-  entrance_d.style.display = "block";
-  necessary_d.style.display = "none";
-  traffic_d.style.display = "none";
-  dormitory_d.style.display = "none";
-  qq_group_d.style.display = "none"; 
+    show(entrance_d);
+    entrance.className = "strategy_way_click";
+    var end = [necessary_d,traffic_d,dormitory_d,qq_d,life_d,food_d,scenery_d];
+    notshow(end);
+    var en = [dormitory,necessary,traffic,qq];
+    changename(en);
 });
+
 traffic.addEventListener("click",function () {
-  traffic_d.style.display = "block";
-  entrance_d.style.display = "none";
-  necessary_d.style.display = "none";
-  dormitory_d.style.display = "none";
-  qq_group_d.style.display = "none"; 
+    show(traffic_d);
+    traffic.className = "strategy_way_click";
+    var tnd = [necessary_d,entrance_d,dormitory_d,qq_d,life_d,food_d,scenery_d];
+    notshow(tnd);
+    var tn = [dormitory,necessary,entrance,qq];
+    changename(tn);
 });
+
 qq.addEventListener("click",function () {
-  qq_group_d.style.display = "block";
-  entrance_d.style.display = "none";
-  necessary_d.style.display = "none";
-  traffic_d.style.display = "none";
-  dormitory_d.style.display = "none"; 
+    show(qq_d);
+    qq.className = "strategy_way_click";
+    var qnd = [dormitory_d,necessary_d,entrance_d,traffic_d,life_d,food_d,scenery_d];
+    notshow(qnd);
+    var qn = [dormitory,necessary,entrance,traffic,];
+    changename(qn);
 });
+
+surround.addEventListener("click",function () {
+    surround.id = "clicked";
+    playCqupt.id = "unclick";
+    show(thrnav);
+    show(life_d);
+    fivnav.style.display = "none";
+    life.className = "su_play_click";
+    var lnd = [dormitory_d,necessary_d,entrance_d,traffic_d,qq_d,food_d,scenery_d];
+    notshow(lnd);
+    var ln = [food,scenery];
+    changeclass(ln);
+});
+
+playCqupt.addEventListener("click",function () {
+    playCqupt.id = "clicked";
+    surround.id = "unclick";
+    show(fivnav);  
+    show(dormitory_d);
+    thrnav.style.display = "none";
+    dormitory.className = "strategy_way_click";
+    var dod = [entrance_d,traffic_d,necessary_d,qq_d,life_d,food_d,scenery_d];
+    notshow(dod);
+    var dor = [necessary,entrance,traffic,qq];
+    changename(dor);
+});
+
+life.addEventListener("click",function () {
+    show(life_d);
+    life.className = "su_play_click";
+    var lnd = [dormitory_d,necessary_d,entrance_d,traffic_d,qq_d,food_d,scenery_d];
+    notshow(lnd);
+    var ln = [food,scenery];
+    changeclass(ln);
+});
+
+food.addEventListener("click",function () {
+    show(food_d);
+    food.className = "su_play_click";
+    var fnd = [dormitory_d,necessary_d,entrance_d,traffic_d,qq_d,life_d,scenery_d];
+    notshow(fnd);
+    var fn = [life,scenery];
+    changeclass(fn);
+});
+
+scenery.addEventListener("click",function () {
+    show(scenery_d);
+    scenery.className = "su_play_click";
+    var snd = [dormitory_d,necessary_d,entrance_d,traffic_d,qq_d,life_d,food_d];
+    notshow(snd);
+    var sd = [life,food];
+    changeclass(sd);
+});
+
+function changeclass(elem) {
+    for (var i = elem.length - 1; i >= 0; i--) {
+        elem[i].className = "su_play";
+    }
+}//重邮周边选项卡切换
+
+function changename(elem) {
+    for (var i = elem.length - 1; i >= 0; i--) {
+        elem[i].className = "strategy_way";
+    }
+}//重邮攻略选项卡切换
+
+function notshow(elem) {
+    for (var i = elem.length - 1; i >= 0; i--) {
+        elem[i].style.display = "none";
+    }
+}//隐藏信息
+
+function show(elem) {
+    elem.style.display = "block";
+}
