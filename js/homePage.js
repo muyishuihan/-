74760adaee24d1ec208fa9e870d05bc4a3007463
yangXiaoHan <540645270@qq.com>
2016-08-06 20:26:40 +0800

    way css beautiful

--- js/homePage.js
+++ js/homePage.js
@@ -164,19 +164,26 @@ var	animated = false;
 	pos_left = [];
 	pos_top = [];
 
-var place_name = ["老图书馆", "太极操场", "老校门", "新校门", "风雨操场", "数字图书馆"]
+var place_name = ["老图书馆", "太极操场", "八十万", "新校门", "风雨操场", "八教", "春华秋实广场", "二教", "红房子", "情人坡", "五栋篮球场", "信科", "逸夫楼"]
 	datails_name_a = [
-		"重庆邮电大学图书馆始建于1950年。现有馆舍20873平方米（含新建的数字图书馆12219平方米）。2002年重庆邮电大学图书馆自动化建设工作被重庆市教委评为优秀馆。", 
-		"操场是供体育锻炼用的场地，多用指学校进行体育活动和教学活动的专置场地。（学习专用）", 
-		"1921年，立飞檐翘角牌坊式校门。五十年代初拆除。2004年年末，为纪念建校一百周年，在原址重建，供观瞻和纪念。", 
-		"我们学校的新校门愉快的暑假结束了,我们回到了久违的学校,我惊喜地发现,我们的校门 发生了巨大的变化", 
-		"《风雨》，《诗经·郑风》篇名。为先秦时代郑地汉族民歌。全诗三章，每章十二字。", 
-		"图书馆分类教育教学图书为主，主要包括教育教学，教育研究，教育管理，课外博览，信息技术，社会科学与哲学，文学，科普，政法，经济，历史，生活，体育，艺术，军事等类图书。",
+		"沉浸于书海，听不见一丝的声音，只剩知识在脑海里交响，喧腾。翻页，亦或是轻轻合拢，用不着辗转反侧，你所思所求，可都在这殿堂中。", 
+		"这里有我们挥汗拼搏的运动赛事，有我们军训铁一般的坚强意志，更有我们开学典礼上充满希望的新奇。", 
+		"据民间神秘知情人士透露，建造八十万花了整整80万大洋。这二楼小洋房般的豪华公厕给予多少邮子便捷，释放了多少匆匆过客的压力！", 
+		"谈不上气派，可就是有一种说不清的韵味。一枚小小的邮票，任邮戳在湛蓝的天空下飞舞。站在门前，享受着微风抚摸脸颊，淡淡的书香，吹进耳朵，变成了闲时嬉戏打闹的声音，不一会儿，就成了运动时的呐喊了。", 
+		"这里是挥洒汗水的场馆，这里是见证友谊的舞台，这个地方神采万千，这个地方激情飞扬，起跳后仰，发力扣杀，双眼从不迷茫，胜利，竭尽全力。",
+		"经典的红绿蓝，高高地矗立在云端。浪漫，文艺，汇集于一体，这梦想的帆船载着重邮人的信念一路向前。", 
+		"当黄桷馨声与草坪音乐的歌声在这里响起，当辛勤的学子从这里经过走进老图，奔向信科...她在这里见证了一代又一代邮子的成长，见证了 一个又一个的成功。",
+		"在咱们这个学术气息浓厚的皇家大重邮，老图和数字图书馆的自习室座位往往是等不到你的到来的，那么这时候位于老图对面，老操场对面的近便的第二教学楼便是你最好的选择。",
+		"这里蕴藏着重邮万丈豪情中的那一抹柔情。这是充满着青春与活力的红色，展示着重邮人蓬勃向上的朝气；这是代表着庄严法律的红色，展示着重邮人踏实律己的作风。",
+		"于百花中寻觅，于千草间踱行。池畔桥旁，莺声燕语；鸾凤和鸣，长歌当行。",
+		"在这里，有排球小将们的身影，有羽球达人们的足迹，更有灌篮高手门的飒爽英姿。",
+		"也许它不是最别致的建筑，但它却是重邮最高（sao）的建筑。这里还聚集着一群程序猿，是工科学子实现梦想的摇篮。",
+		"这里有着其他地方无可比拟的厚度，一个甲子的沉淀，使得历史的气息从一进门便扑面而来，校史馆的每一件展品都藏着一个老故事，只要你看过，就有动力为重邮未来添上一笔。",
 	]
 
 for (var i = 0; i < place_position.children.length; i++) {
 	pos_left[i] = parseFloat(place_position.children[i].offsetLeft) + 40;
-	pos_top[i] = parseFloat(place_position.children[i].offsetTop) + 20;
+	pos_top[i] = parseFloat(place_position.children[i].offsetTop) - 20;
 }
 
 //详细建筑框的显示
@@ -196,7 +203,7 @@ place_position.addEventListener("mouseover", function(e){
 				place.innerHTML = place_name[1];
 				list.style.left = -930 + "px";
 			break;
-		case "oldDoor-pos":
+		case "eightyWc-pos":
 				place.innerHTML = place_name[2];
 				list.style.left = -1395 + "px";
 			break;
@@ -208,72 +215,125 @@ place_position.addEventListener("mouseover", function(e){
 				place.innerHTML = place_name[4];
 				list.style.left = -2325 + "px";
 			break;
-		case "newLib-pos":
+		case "eightBuilding-pos":
 				place.innerHTML = place_name[5];
 				list.style.left = -2790 + "px";
+			break;
+		case "spring-pos":
+				place.innerHTML = place_name[6];
+				list.style.left = -3255 + "px";
+			break;
+		case "twoBuilding-pos":
+				place.innerHTML = place_name[7];
+				list.style.left = -3720 + "px";
+			break;
+		case "redBuilding-pos":
+				place.innerHTML = place_name[8];
+				list.style.left = -4185 + "px";
+			break;
+		case "love-pos":
+				place.innerHTML = place_name[9];
+				list.style.left = -4650 + "px";
+			break;
+		case "basketball-pos":
+				place.innerHTML = place_name[10];
+				list.style.left = -5115 + "px";
+			break;
+		case "shineford-pos":
+				place.innerHTML = place_name[11];
+				list.style.left = -5580 + "px";
+			break;
+		case "houseBuilding-pos":
+				place.innerHTML = place_name[12];
+				list.style.left = -6045 + "px";
 			break;
 	}
 
 	place.style.left = parseFloat(e.target.offsetLeft) + 40 + "px";
-	place.style.top = parseFloat(e.target.offsetTop) + 20 + "px";
+	place.style.top = parseFloat(e.target.offsetTop) - 20  + "px";
 
 })
 
 //详细建筑框的隐藏和内容的显示
 place_position.addEventListener("click", function(e){
-
 	details.style.display = "block";
 
-	switch (e.target.id) {
+		switch (e.target.id) {
 		case "old-pos":
 				datails_name.innerHTML = place_name[0];
-				datails_write.innerHTML = datails_name_a[0]
-				list.style.left = -465 + "px";
+				datails_write.innerHTML = datails_name_a[0];
 				index = 0;
 			break;
 		case "taiJi-pos":
 				datails_name.innerHTML = place_name[1];
-				datails_write.innerHTML = datails_name_a[1]
-				list.style.left = -930 + "px";
+				datails_write.innerHTML = datails_name_a[1];
 				index = 1;
 			break;
-		case "oldDoor-pos":
+		case "eightyWc-pos":
 				datails_name.innerHTML = place_name[2];
-				datails_write.innerHTML = datails_name_a[2]
-				list.style.left = -1395 + "px";
+				datails_write.innerHTML = datails_name_a[2];
 				index = 2;
 			break;
 		case "newDoor-pos":
 				datails_name.innerHTML = place_name[3];
-				datails_write.innerHTML = datails_name_a[3]
-				list.style.left = -1860 + "px";
+				datails_write.innerHTML = datails_name_a[3];
 				index = 3;
 			break;
 		case "windRain-pos":
 				datails_name.innerHTML = place_name[4];
-				datails_write.innerHTML = datails_name_a[4]
-				list.style.left = -2325 + "px";
+				datails_write.innerHTML = datails_name_a[4];
 				index = 4;
 			break;
-		case "newLib-pos":
+		case "eightBuilding-pos":
 				datails_name.innerHTML = place_name[5];
-				datails_write.innerHTML = datails_name_a[5]
-				list.style.left = -2790 + "px";
+				datails_write.innerHTML = datails_name_a[5];
 				index = 5;
 			break;
+		case "spring-pos":
+				datails_name.innerHTML = place_name[6];
+				datails_write.innerHTML = datails_name_a[6];
+				index = 6;
+			break;
+		case "twoBuilding-pos":
+				datails_name.innerHTML = place_name[7];
+				datails_write.innerHTML = datails_name_a[7];
+				index = 7;
+			break;
+		case "redBuilding-pos":
+				datails_name.innerHTML = place_name[8];
+				datails_write.innerHTML = datails_name_a[8];
+				index = 8;
+			break;
+		case "love-pos":
+				datails_name.innerHTML = place_name[9];
+				datails_write.innerHTML = datails_name_a[9];
+				index = 9;
+			break;
+		case "basketball-pos":
+				datails_name.innerHTML = place_name[10];
+				datails_write.innerHTML = datails_name_a[10];
+				index = 10;
+			break;
+		case "shineford-pos":
+				datails_name.innerHTML = place_name[11];
+				datails_write.innerHTML = datails_name_a[11];
+				index = 11;
+			break;
+		case "houseBuilding-pos":
+				datails_name.innerHTML = place_name[12];
+				datails_write.innerHTML = datails_name_a[12];
+				index = 12;
+			break;
 	}
-	place.style.left = parseFloat(e.target.offsetLeft) + 40 + "px";
-	place.style.top = parseFloat(e.target.offsetTop) + 20 + "px";
-
-})
+});
 
 
 //详细建筑框的轮播
 function animate(offset){
 	animated = true;
 	var newleft = parseInt(list.style.left) + offset;
-	var time = 300;  //位移总的时间
-	var interval = 20;  //位移间隔
+	var time = 465;  //位移总的时间
+	var interval = 5;  //位移间隔
 	var speed = offset/(time/interval);  //每次位移量
 
 	function go() {
@@ -284,9 +344,9 @@ function animate(offset){
 			animated = false;
 			list.style.left = newleft + 'px';
 			if (newleft > -465) {
-				list.style.left = -2790 + "px";
+				list.style.left = -6045 + "px";
 			}
-			if (newleft < -2790) {
+			if (newleft < -6045) {
 				list.style.left = -465 + "px";
 			}
 		}
@@ -294,13 +354,12 @@ function animate(offset){
 	go();
 }
 next.addEventListener("click", function () {
-	if (index == 5) {
+	if (index == 12) {
 		index = 0;
 	}else{
 		index += 1;
 	}
 	if (!animated) {
-		console.log(index)
 		animate(-465);
 		datails_name.innerHTML = place_name[index];
 		datails_write.innerHTML = datails_name_a[index];
@@ -310,7 +369,7 @@ next.addEventListener("click", function () {
 
 prev.addEventListener("click", function () {
 	if (index == 0) {
-		index = 5;
+		index = 12;
 	}else{
 		index -= 1;
 	}
@@ -325,34 +384,69 @@ prev.addEventListener("click", function () {
 var ani_pos = function (num) {
 	switch (num) {
 		case 0:
-			place.style.left = pos_left[0] + "px";
-			place.style.top = pos_top[0] + "px";
-			place.innerHTML = place_name[0];
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
 			break;
 		case 1:
-			place.style.left = pos_left[1] + "px";
-			place.style.top = pos_top[1] + "px";
-			place.innerHTML = place_name[1];
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
 			break;
 		case 2:
-			place.style.left = pos_left[2] + "px";
-			place.style.top = pos_top[2] + "px";
-			place.innerHTML = place_name[2];
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
 			break;
 		case 3:
-			place.style.left = pos_left[3] + "px";
-			place.style.top = pos_top[3] + "px";
-			place.innerHTML = place_name[3];
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
 			break;
 		case 4:
-			place.style.left = pos_left[4] + "px";
-			place.style.top = pos_top[4] + "px";
-			place.innerHTML = place_name[4];
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
 			break;
 		case 5:
-			place.style.left = pos_left[5] + "px";
-			place.style.top = pos_top[5] + "px";
-			place.innerHTML = place_name[5];
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
+			break;
+		case 6:
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
+			break;
+		case 7:
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
+			break;
+		case 8:
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
+			break;
+		case 9:
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
+			break;
+		case 10:
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
+			break;
+		case 11:
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
+			break;
+		case 12:
+			place.style.left = pos_left[num] + "px";
+			place.style.top = pos_top[num] + "px";
+			place.innerHTML = place_name[num];
 			break;
 	}
 }
@@ -377,29 +471,29 @@ big_button.addEventListener("click", function(){
 	place_position.style.display = "none";
 
 	switch (parseFloat(getStyle(place,"left"))) {
-		case 560:
-			big_map.style.left = -1194 + "px";
-			big_map.style.top = -1135 + "px";
+		case 580 || 495 || 520 || 540 || 640://老图，二教，春华秋实，红房子，情人坡
+			big_map.style.left = -1209 + "px"; //ok
+			big_map.style.top = -1117 + "px";
 			break;
-		case 436:
-			big_map.style.left = -828 + "px";
-			big_map.style.top = -552 + "px";
+		case 436 || 350 || 460: //太极，风雨，篮球
+			big_map.style.left = -947 + "px";//ok
+			big_map.style.top = -644 + "px";
 			break;
-		case 410:
-			big_map.style.left = -879 + "px";
-			big_map.style.top = -1465 + "px";
+		case 450 || 230: //八十万，信科
+			big_map.style.left = -995 + "px";//ok
+			big_map.style.top = -1401 + "px";
 			break;
-		case 260:
+		case 270://新校门
 			big_map.style.left = -434 + "px";
 			big_map.style.top = -1361 + "px";
 			break;
-		case 330:
-			big_map.style.left = -517 + "px";
-			big_map.style.top = -727 + "px";
+		case 310: //八教
+			big_map.style.left = -454 + "px";//ok
+			big_map.style.top = -302 + "px";
 			break;
-		case 310:
-			big_map.style.left = -391 + "px";
-			big_map.style.top = -1228 + "px";
+		case 230: //逸夫楼
+			big_map.style.left = -312 + "px";
+			big_map.style.top = -925 + "px";
 			break;
 	}
 })
@@ -423,6 +517,7 @@ function mousemove(){
 	{	
 		big_map.style.left = (window.event.x - X1) + "px";
 		big_map.style.top = (window.event.y - Y1) + "px";
+
 		if ((window.event.x - X1) < -2539) {
 			big_map.style.left = -2539 + "px";
 		}
--- js/way.js
+++ js/way.js
@@ -139,3 +139,402 @@ function notshow(elem) {
 function show(elem) {
     elem.style.display = "block";
 }
+
+
+
+
+
+
+//封装
+var getStyle = function(dom,attr){
+    return dom.currentStyle ? dom.currentStyle[attr] : getComputedStyle(dom, false)[attr];
+}
+
+var $ = function (event) {
+    return document.querySelector(event);
+}
+
+function indexOf(arr,value,start){
+  //如果不设置start,则默认start为0
+  if(arguments.length == 2){
+    start = 0;
+  }
+  //如果数组中存在indexOf方法，则用原生的indexOf方法
+  if(arr.indexOf){
+    return arr.indexOf(value,start);
+  }
+  for( var i = 0; i < arr.length; i++){
+    if(arr[i] === value){
+      return i;
+    }
+  }
+  return -1;
+}
+//数组去重方法封装
+function noRepeat(arr){
+  var result = [];
+  for( var i = 0; i < arr.length; i++){
+    if(indexOf(result,arr[i]) == -1){
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+//inArray方法封装
+function inArray(arr,value){
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] === value){
+      return true;
+    }
+  }
+  return false;
+}
+//去除首尾空格函数封装
+function trim(arr){
+  var result = arr.replace(/^\s+|\s+$/g,'');
+  return result;
+}
+function addClass(obj,classStr){
+  var array = noRepeat(trim(obj.className).split('\s+'));
+  if(!inArray(array,classStr)){
+    array.push(classStr);
+  }
+  obj.className = array.join(' ');
+  return obj;
+}
+
+function removeClass(obj,classStr){
+  var array = noRepeat(trim(obj.className).split('\s+'));
+  var index = indexOf(array,classStr);
+  if(index != -1){
+    array.splice(index,1);
+    obj.className = array.join(' ');
+  }
+  return obj;
+}
+
+//飞机和回顶按钮的隐藏
+var tt = 500;
+    fix_l = $(".fix-l")
+    fix_r = $(".fix-r");
+
+window.onscroll = function(){  //兼容ie8
+    var t = document.documentElement.scrollTop || document.body.scrollTop;
+        bottom_t = document.documentElement.offsetHeight || document.body.offsetHeight;
+        bottom_b = document.documentElement.clientHeight || document.body.clientHeight;
+        bottom = bottom_t - bottom_b - t;
+
+    if (bottom <= 10) {
+        backTop.style.bottom = (-bottom) + 70 + "px";
+    }else {
+        backTop.style.bottom = 30 + "px";
+    }
+
+    if(t < tt && getStyle(backTop,"opacity")!="0"){
+        backTop.style.opacity="0";
+    }else if(t > tt && getStyle(backTop,"opacity")=="0"){
+        backTop.style.opacity="1";
+    }else{
+        return false;
+    }
+
+    if(t < 1500 && getStyle(fix_l,"opacity")!="1"){
+        fix_l.style.opacity="1";
+    }else if(t > tt && getStyle(fix_l,"opacity")=="1"){
+        fix_l.style.opacity="0";
+    }else{
+        return false;
+    }
+
+    if(t < 1500 && getStyle(fix_r,"opacity")!="1"){
+        fix_r.style.opacity="1";
+    }else if(t > tt && getStyle(fix_r,"opacity")=="1"){
+        fix_r.style.opacity="0";
+    }else{
+        return false;
+    }
+}
+
+
+//回到顶部
+var backTop = $(".back"); //回顶按钮
+    backTop.addEventListener("click", back)
+function back() {
+    var t = document.documentElement.scrollTop || document.body.scrollTop;
+        speed = 100;  //每次位移量
+        interval = 20
+    if (t > 0) {
+        document.documentElement.scrollTop = t - speed;
+        document.body.scrollTop = t - speed;
+        setTimeout(back, interval);
+    } 
+}
+
+//计时器
+var timer = [$(".day-l"), $(".day-r"), $(".hour-l"), $(".hour-r"), $(".min-l"), $(".min-r"), $(".sec-l"), $(".sec-r")]
+    startTime = new Date().getTime();
+    timeNew = new Date();
+    month = timeNew.getMonth() + 1;
+    year = timeNew.getFullYear()
+    day = 0;
+    count = 0;
+    stop = 0;
+
+function change(event, num){
+    if (num == 0) {
+        event.style.top = 0 + "px";
+    }
+    if (num == 1) {
+        event.style.top = -37 + "px";
+    }
+    if (num == 2) {
+        event.style.top = -73 + "px";
+    }
+    if (num == 3) {
+        event.style.top = -110 + "px";
+    }
+    if (num == 4) {
+        event.style.top = -147 + "px";
+    }
+    if (num == 5) {
+        event.style.top = -183 + "px";
+    }
+    if (num == 6) {
+        event.style.top = -220 + "px";
+    }
+    if (num == 7) {
+        event.style.top = -257 + "px";
+    }
+    if (num == 8) {
+        event.style.top = -293 + "px";
+    }
+    if (num == 9) {
+        event.style.top = -330 + "px";
+    }
+
+}
+
+if (month == 8 && year == 2016){
+    day = 31+5;
+}else if (month == 9 && year == 2016) {
+    day = 5;
+}else {
+    stop = 1
+}
+
+function timeChange(stop) {
+    count++;
+
+    var offset = new Date().getTime() - (startTime + count * 1000);
+        nextTime = 1000 - offset;
+        timeNew = new Date();
+        sec = timeNew.getSeconds();
+        min = timeNew.getMinutes();
+        hour = timeNew.getHours();
+        dayNew = day - timeNew.getDay();
+
+    if (nextTime < 0) nextTime = 0;
+
+    if (stop != 1) {
+        var time = [parseInt((dayNew%100)/10), dayNew%10, (2 - parseInt((hour%100)/10)), (4 - hour%10), (5 - parseInt((min%100)/10)), (9 - min%10), (5 - parseInt((sec%100)/10)), (9 - sec%10)]
+            length = timer.length
+
+        for (var i = 0; i < length; i++) {
+            change(timer[i], time[i])
+        }
+        
+    }else{
+        for (var i = 0; i < length; i++) {
+            change(timer[i], 0);
+        }
+    }
+    setTimeout(timeChange, nextTime);
+}
+setTimeout("timeChange(" + stop + ")", 1000);
+
+
+var left_arr = $("#left_arr");
+    right_arr = $("#right_arr");
+    list = $(".list");
+    index_ani = 0;
+    animated = false;
+
+
+function animate(offset){
+    animated = true;
+    var newleft = parseInt(list.style.left) + offset;
+    var time = 500;  //位移总的时间
+    var interval = 5;  //位移间隔
+    var speed = offset/(time/interval);  //每次位移量
+
+    function go() {
+        if ((speed < 0 && parseInt(list.style.left) > newleft) || (speed > 0 && parseInt(list.style.left) < newleft)){
+            list.style.left = parseInt(list.style.left) + speed + "px";
+            setTimeout(go, interval);
+        }else{
+            animated = false;
+            list.style.left = newleft + 'px';
+            if (newleft > -428) {
+                list.style.left = -1712 + "px";
+            }
+            if (newleft < -1712) {
+                list.style.left = -428 + "px";
+            }
+        }
+    }
+    go();
+}
+right_arr.addEventListener("click", function () {
+    if (index_ani == 3) {
+        index_ani = 0;
+    }else{
+        index_ani += 1;
+    }
+    if (!animated) {
+        animate(-428);
+    }
+});
+
+left_arr.addEventListener("click", function () {
+    if (index_ani == 0) {
+        index_ani = 3;
+    }else{
+        index_ani -= 1;
+    }
+    if (!animated) {
+        animate(428);
+    }
+});
+
+//滚动条
+var flaot_d = $(".flaot_d");
+    float_line = $(".float_line");
+    entr_flaot_d = $(".entr_flaot_d");
+    qq_det = $("#qq_det");
+    must_det = $("#must_det");
+    move1 = 0;
+    move2 = 0;
+
+qq_det.onscroll = function(){
+    var t = qq_det.scrollTop;
+    flaot_d.style.top = (t/(1268/399)) + "px";
+}
+must_det.onscroll = function(){
+    var t = must_det.scrollTop;
+    entr_flaot_d.style.top = (t/(9919/628)) + "px";
+    four_buttom();
+}
+
+//须知四个按钮变色
+function four_buttom() {
+    var top = entr_flaot_d.offsetTop;
+        four_new = $("#left_side_det").children;
+        num = 0;
+    if (top >= 0 && top < 380) {
+        num = 0;
+        four_new[1].style.background = "#fed18d"
+        four_new[2].style.background = "#fed18d"
+        four_new[3].style.background = "#fed18d"
+    }
+    if (top >= 380 && top < 419) {
+        num = 1;
+        four_new[0].style.background = "#fed18d"
+        four_new[2].style.background = "#fed18d"
+        four_new[3].style.background = "#fed18d"
+    }
+    if (top >= 419 && top < 542) {
+        num = 2;
+        four_new[0].style.background = "#fed18d"
+        four_new[1].style.background = "#fed18d"
+        four_new[3].style.background = "#fed18d"
+    }
+    if (top >= 542 && top < 628) {
+        num = 3;
+        four_new[0].style.background = "#fed18d"
+        four_new[1].style.background = "#fed18d"
+        four_new[2].style.background = "#fed18d"
+    }
+    four_new[num].style.background = "#9ae4e9"
+}
+
+//须知按动滚
+function mousedown1() {
+    Y1 = window.event.y - parseInt(entr_flaot_d.offsetTop);
+    move1 = 1;
+}
+function mousestop1() {
+    window.event.returnValue = false;
+}
+function mousemove1(){
+    if (move1 == 1)
+    {   
+        entr_flaot_d.style.top = (window.event.y - Y1) + "px";
+        must_det.scrollTop = ((9919/628)*(window.event.y - Y1));
+        addClass(must_det, "way_masking");
+        four_buttom();
+        if ((window.event.y - Y1) < 0) {
+            entr_flaot_d.style.top = 0 + "px";
+            must_det.scrollTop = 0;
+        }
+        if (entr_flaot_d.offsetTop > 628) {
+            entr_flaot_d.style.top = 628 + "px";
+            must_det.scrollTop = 1268;
+        }
+    }
+}
+function mouseup1(){
+    removeClass(must_det, "way_masking");
+    removeClass(qq_det, "way_masking");
+    move1 = 0;
+}
+function remove1(){
+    entr_flaot_d.onmousemove = mousemove1;
+    entr_flaot_d.onmousedown = mousedown1;
+    entr_flaot_d.onmouseup = mouseup1;
+    entr_flaot_d.ondragstart  = mousestop1;
+
+    must_det.onmousemove = mousemove1;
+    must_det.onmouseup = mouseup1;
+    must_det.ondragstart  = mousestop1;
+}
+remove1()
+
+
+//qq按动滚
+function mousedown2() {
+    Y1 = window.event.y - parseInt(flaot_d.offsetTop);
+    move2= 1;
+}
+function mousestop2() {
+    window.event.returnValue = false;
+}
+function mousemove2(){
+    if (move2 == 1)
+    {   
+        flaot_d.style.top = (window.event.y - Y1) + "px";
+        qq_det.scrollTop = ((1268/399)*(window.event.y - Y1));
+        addClass(qq_det, "way_masking");
+        if ((window.event.y - Y1) < 0) {
+            flaot_d.style.top = 0 + "px";
+            qq_det.scrollTop = 0;
+        }
+        if ((window.event.y - Y1) > 399) {
+            flaot_d.style.top = 399 + "px";
+            qq_det.scrollTop = 1268;
+        }
+    }
+}
+function mouseup2(){
+    move2 = 0;
+}
+function remove2(){
+    flaot_d.onmousemove = mousemove2;
+    flaot_d.onmousedown = mousedown2;
+    flaot_d.onmouseup = mouseup2;
+    flaot_d.ondragstart  = mousestop2;
+
+    qq_det.onmousemove = mousemove2;
+    qq_det.onmouseup = mouseup2;
+    qq_det.ondragstart  = mousestop2;
+}
+remove2()
